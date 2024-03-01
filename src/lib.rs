66fda0447788076b7d0fb2274af05bdec2ae8893
rmqtt <rmqttd@126.com>
2024-03-01 21:57:14 +0800

    Update the test codes

--- src/lib.rs
+++ src/lib.rs
@@ -1595,7 +1595,12 @@ mod tests {
     #[test]
     async fn test_db_size() {
         let cfg = get_cfg("db_size");
-        let db = init_db(&cfg).await.unwrap();
+        let mut db = init_db(&cfg).await.unwrap();
+        let iter = db.scan("*").await.unwrap();
+        for item in collect(iter).await {
+            db.remove(item).await.unwrap();
+        }
+
         db.insert("k1", &1).await.unwrap();
         db.insert("k2", &2).await.unwrap();
         db.insert("k3", &3).await.unwrap();
@@ -1604,6 +1609,23 @@ mod tests {
         m.insert("mk1", &1).await.unwrap();
         m.insert("mk2", &2).await.unwrap();
         println!("test_db_size db_size: {:?}", db.db_size().await);
+
+        db.batch_insert(vec![
+            (Vec::from("batch/len/1"), 11),
+            (Vec::from("batch/len/2"), 22),
+            (Vec::from("batch/len/3"), 33),
+        ])
+        .await
+        .unwrap();
+        println!("test_db_size db_size: {:?}", db.db_size().await);
+    }
+
+    async fn collect(mut iter: Box<dyn AsyncIterator<Item = Result<Key>> + Send + '_>) -> Vec<Key> {
+        let mut data = Vec::new();
+        while let Some(key) = iter.next().await {
+            data.push(key.unwrap())
+        }
+        data
     }
 
     #[tokio::main]
@@ -1646,21 +1668,6 @@ mod tests {
             }
         };
 
-        async fn collect(
-            mut iter: Box<dyn AsyncIterator<Item = Result<Key>> + Send + '_>,
-        ) -> Vec<Key> {
-            let mut data = Vec::new();
-            while let Some(key) = iter.next().await {
-                let key = key.unwrap();
-                println!(
-                    "test_scan next: key: {:?}",
-                    String::from_utf8_lossy(key.as_slice())
-                );
-                data.push(key)
-            }
-            data
-        }
-
         //foo/abcd*
         let topic = format_topic("foo/abcd/#");
         println!("topic: {}", topic);
@@ -1696,5 +1703,61 @@ mod tests {
         println!("---topic: {} {}---", topic, "iot/abcd/*");
         let iter = db.scan(topic.as_bytes()).await.unwrap();
         assert_eq!(collect(iter).await.len(), 4);
+    }
+
+    #[tokio::main]
+    #[cfg(feature = "len")]
+    #[test]
+    async fn test_len() {
+        let cfg = get_cfg("len");
+        let mut db = init_db(&cfg).await.unwrap();
+        println!("a test_len len: {:?}", db.len().await);
+        let iter = db.scan("*").await.unwrap();
+        for item in collect(iter).await {
+            println!(
+                "test_len remove item: {:?}",
+                String::from_utf8_lossy(item.as_slice())
+            );
+            db.remove(item).await.unwrap();
+        }
+        println!("b test_len len: {:?}", db.len().await);
+        db.insert("foo/len/1", &1).await.unwrap();
+        db.insert("foo/len/2", &2).await.unwrap();
+        db.insert("foo/len/3", &3).await.unwrap();
+        db.insert("foo/len/4", &4).await.unwrap();
+
+        db.expire_at("foo/len/3", timestamp_millis() + 1 * 1000)
+            .await
+            .unwrap();
+        db.expire("foo/len/4", 1000 * 2).await.unwrap();
+        println!("test_len len: {:?}", db.len().await);
+        assert_eq!(db.len().await.unwrap(), 4);
+
+        sleep(Duration::from_millis(1100)).await;
+        println!("test_len len: {:?}", db.len().await);
+        assert_eq!(db.len().await.unwrap(), 3);
+
+        sleep(Duration::from_millis(1100)).await;
+        println!("test_len len: {:?}", db.len().await);
+        assert_eq!(db.len().await.unwrap(), 2);
+
+        db.remove("foo/len/1").await.unwrap();
+        println!("test_len len: {:?}", db.len().await);
+        assert_eq!(db.len().await.unwrap(), 1);
+
+        db.batch_insert(vec![
+            (Vec::from("batch/len/1"), 11),
+            (Vec::from("batch/len/2"), 22),
+            (Vec::from("batch/len/3"), 33),
+        ])
+        .await
+        .unwrap();
+        assert_eq!(db.len().await.unwrap(), 4);
+
+        db.batch_remove(vec![Vec::from("batch/len/1"), Vec::from("batch/len/2")])
+            .await
+            .unwrap();
+
+        assert_eq!(db.len().await.unwrap(), 2);
     }
 }
